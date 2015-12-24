b16ff94b7a239cf31b67ecc63df8e5ee5f9fddf1
Kalman Olah <hello@kalmanolah.net>
2015-12-24 23:26:15 +0100

    GPIO: GPIO module added, for basic control (setting, pulsing) of IOs.
    Signed-off-by: Kalman Olah <hello@kalmanolah.net>

---
+++ src/module/gpio.lua
@@ -0,0 +1,33 @@
+-- This module adds commands for controlling IOs.
+local obj = {}
+
+obj.command_handlers = function()
+  local handlers = {}
+
+  handlers['/gpio/control'] = function (evt)
+    local pin = evt.data.pin
+    local state = evt.data.state == true and gpio.HIGH or gpio.LOW
+
+    gpio.mode(pin, gpio.OUTPUT)
+
+    if (not evt.data.type) or (evt.data.type == 'set') then
+      gpio.write(pin, state)
+    elseif evt.data.type == 'pulse' then
+      gpio.write(pin, state)
+      tmr.delay(evt.data.duration and (evt.data.duration * 1000) or 500000)
+      gpio.write(pin, state == gpio.HIGH and gpio.LOW or gpio.HIGH)
+    end
+
+    pin = nil
+    state = nil
+    collectgarbage()
+  end
+
+  return handlers
+end
+
+return function(fnc, args)
+  if obj[fnc] then
+    return obj[fnc](args)
+  end
+end
