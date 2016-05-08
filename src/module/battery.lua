9baf5339506c4a437dbf5c41c75e09ae65474724
Kalman Olah <hello@kalmanolah.net>
2016-05-08 17:00:12 +0200

    In recent firmware, executing adc.readvdd33() while already connected to Wifi is possible.
    Signed-off-by: Kalman Olah <hello@kalmanolah.net>

--- src/module/battery.lua
+++ src/module/battery.lua
@@ -1,20 +1,11 @@
--- This module adds battery voltage to reports. Due to certain
--- hardware limitations, battery voltage is only measured at boot.
+-- Module adding battery voltage to reports.
 local obj = {}
 
-obj._init = function()
-  obj.reading = adc.readvdd33()
-end
-
 obj._report_data = function()
-  if not obj.reading then
-    return
-  end
-
-  local reading_str = string.format("%d.%03d", math.floor(obj.reading / 1000), obj.reading - ((obj.reading / 1000) * 1000))
+  local r = adc.readvdd33()
 
   return {
-    { "/sensors/battery/voltage", reading_str }
+    { "/sensors/battery/voltage", string.format("%d.%03d", math.floor(r / 1000), r - ((r / 1000) * 1000)) }
   }
 end
 
