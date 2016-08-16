e269a5603d2af4e7b2fed0da5d161538bf59e9e1
Kalman Olah <hello@kalmanolah.net>
2016-08-16 20:10:24 +0200

    Initial Atlas Scientic EZO (tm) ORP circuit module. Modified pH module a bit as well.
    Signed-off-by: Kalman Olah <hello@kalmanolah.net>

--- src/module/atlas_ezo_orp.lua
+++ src/module/atlas_ezo_orp.lua
@@ -1,18 +1,18 @@
--- Atlas Scientific EZO (tm) pH module
--- http://atlas-scientific.com/product_pages/circuits/ezo_ph.html
-ezoph = {
+-- Atlas Scientific EZO (tm) ORP module
+-- http://atlas-scientific.com/product_pages/circuits/ezo_orp.html
+ezoorp = {
   sda = nil,
   scl = nil,
   cmd = function(cmd)
-    if not (ezoph.sda and ezoph.scl) then
+    if not (ezoorp.sda and ezoorp.scl) then
       return
     end
 
     local id = 0
-    local d = 0x63
+    local d = 0x62
     local r = nil
 
-    i2c.setup(id, ezoph.sda, ezoph.scl, i2c.SLOW)
+    i2c.setup(id, ezoorp.sda, ezoorp.scl, i2c.SLOW)
     i2c.start(id)
     i2c.address(id, d, i2c.TRANSMITTER)
     i2c.write(id, cmd)
@@ -36,11 +36,11 @@ ezoph = {
 
 _k.on('_cfg.fields', function ()
   return {
-    ezoph_sda_pin = {
+    ezoorp_sda_pin = {
       type = 'number',
       default = false
     },
-    ezoph_scl_pin = {
+    ezoorp_scl_pin = {
       type = 'number',
       default = false
     }
@@ -48,23 +48,22 @@ _k.on('_cfg.fields', function ()
 end)
 
 _k.on('_init', function ()
-  ezoph.sda = cfg.data.ezoph_sda_pin
-  ezoph.scl = cfg.data.ezoph_scl_pin
-  ezoph.cmd('L,0')
+  ezoorp.sda = cfg.data.ezoorp_sda_pin
+  ezoorp.scl = cfg.data.ezoorp_scl_pin
+  ezoorp.cmd('L,0')
 end)
 
 _k.on('_report', function ()
-  local r = ezoph.cmd('R')
-  ezoph.cmd('SLEEP')
+  local r = ezoorp.cmd('R')
+  ezoorp.cmd('SLEEP')
 
   if r ~= nil then
-    return {{ "/sensors/ezoph/ph", r }}
+    return {{ "/sensors/ezoorp/orp", r }}
   end
 end)
 
-_k.cmd('ezoph/calibrate', function (e)
-  local ph = e.ph or 7
-  local lvl = e.lvl or 'mid'
-  ezoph.cmd('Cal,'..lvl..','..ph..'.00')
-  ezoph.cmd('SLEEP')
+_k.cmd('ezoorp/calibrate', function (e)
+  local v = e.value
+  ezoorp.cmd(string.format("Cal,%d.%03d", math.floor(v / 1000), v - ((v / 1000) * 1000)))
+  ezoorp.cmd('SLEEP')
 end)
